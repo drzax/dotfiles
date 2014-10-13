4835ad21eacb495c2abe8654e678c668aba43cb6
Simon Elvery <simon@elvery.net>
2014-10-13 20:13:34 +1000

    abc-interactive grunt-init

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -57,4 +57,4 @@ if [[ "$(type -P gem)" ]]; then
 fi
 
 # Get or update all the grunt-init templates
-clone_or_pull https://github.com/drzax/grunt-init-app.git ~/.grunt-init/quick-start
+clone_or_pull git@bitbucket.org:abcdevelop/news-template-interactive-gruntinit.git ~/.grunt-init/abc-interactive
