c5a06b709444e98cdb260ca2b460f3c9fcfd7880
Simon Elvery <simon@elvery.net>
2014-11-18 10:12:15 +1000

    Proxyless.

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -58,3 +58,6 @@ fi
 
 # Get or update all the grunt-init templates
 clone_or_pull git@bitbucket.org:abcdevelop/news-template-interactive-gruntinit.git ~/.grunt-init/abc-interactive
+
+# ABC Proxyless
+clone_or_pull git@bitbucket.org:abcdevelop/news-proxyless-util.git ~/proxyless
--- source/50_devel.sh
+++ source/50_devel.sh
@@ -115,3 +115,5 @@ function pngcrushdir() {
     mv -f temp.png $png
   done;
 }
+
+alias prx='~/proxyless/proxyless-command'
