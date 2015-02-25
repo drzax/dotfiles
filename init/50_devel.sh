4f735a74bb702be45f2543deb8f56674f66d3058
Simon Elvery <simon@elvery.net>
2015-02-25 20:27:13 +1000

    Added atom config

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -3,3 +3,34 @@ clone_or_pull git@bitbucket.org:abcdevelop/news-template-interactive-gruntinit.g
 
 # ABC Proxyless
 clone_or_pull git@bitbucket.org:abcdevelop/news-proxyless-util.git ~/proxyless
+
+# Exit if Atom is not installed.
+[[ ! "$(type -P apm)" ]] && e_error "Atom packages need Atom (and apm) to install." && return 1
+
+packages=(
+  linter
+  merge-conflicts
+)
+
+# This removes the version numbers from the output of `apm list --installed --bare`
+function remove_versions() {
+  local list out
+  list=($1)
+  out=(${list[@]//@*})
+  echo "${out[@]}"
+}
+
+# Install Atom packages.
+function apm_install_packages() {
+  local current
+  current=$(remove_versions "$(apm list --installed --bare)")
+  packages=($(setdiff "${packages[*]}" "${current[*]}"))
+  if (( ${#packages[@]} > 0 )); then
+    e_header "Installing Atom modules: ${packages[*]}"
+    for package in "${packages[@]}"; do
+      apm install $package
+    done
+  fi
+}
+
+apm_install_packages
