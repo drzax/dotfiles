1fb1e6920c1443a5de977bf71634b43d6afe82c1
Simon Elvery <simon@elvery.net>
2016-03-24 17:08:09 +1000

    Better todo.txt things

--- init/50_osx.sh
+++ init/50_osx.sh
@@ -1,3 +1,4 @@
 # Install todo.txt add-ons.
 clone_or_pull git@github.com:timpulver/todo.txt-graph.git ~/.todo.actions.d/graph
 clone_or_pull git@github.com:drzax/todo-view.git ~/.todo.actions.d/view
+clone_or_pull git@github.com:drzax/todo-future.git ~/.todo.actions.d/future
