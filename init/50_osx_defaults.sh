4b0e9d036cb134813a079953e4742821d4b61a8a
Simon Elvery <simon@elvery.net>
2019-07-17 16:47:37 +1000

    Update locale comments

--- init/50_osx_defaults.sh
+++ init/50_osx_defaults.sh
@@ -187,8 +187,6 @@ defaults write com.apple.BezelServices kDim -bool true
 defaults write com.apple.BezelServices kDimTime -int 300
 
 # Set language and text formats
-# Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
-# `Inches`, `en_GB` with `en_US`, and `true` with `false`.
 defaults write NSGlobalDomain AppleLanguages -array "en"
 defaults write NSGlobalDomain AppleLocale -string "en_AU@currency=AUD"
 defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
